41e62e0e9c062da4fa85bbf2e161364bcb27c691
Nathan Schrader <nathanschrader@icloud.com>
2021-03-13 09:35:30 -0800

    add file structure

---
+++ src/components/products/Product/Product.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+//These are named imports, so all have to be named exact
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+// import classes from '*.module.css';
+import useStyles from './styles';
+
+const Product = ({ product }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={ classes.root }>
+      <CardMedia className={ classes.media } image='' title={ product.name }/>
+      <CardContent>
+        <div className={ classes.cardContent }>
+          <Typography variant='h5' gutterBottom>
+            { product.name }
+          </Typography>
+          <Typography variant='h5'>
+            { product.price }
+          </Typography>
+        </div>
+        <Typography variant='h2' color='textSecondary'>{ product.description }</Typography>
+        <CardActions disableSpacing className={ classes.cardActions }>
+          <IconButton aria-label='Add to Card'>
+            <AddShoppingCard />
+          </IconButton>
+        </CardActions>
+      </CardContent>
+
+    </Card>
+  )
+}
+
+export default Product
